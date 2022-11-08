17142f5423c8846364942873c1cb25747c59eb18
Rania BEN TEKAYA <117775943+bentekaya@users.noreply.github.com>
2022-11-08 23:35:32 +0100

    Update haarrcascade.py

--- haarrcascade.py
+++ haarrcascade.py
@@ -9,4 +9,10 @@ import CV2
 from firebase import firebase
 firebase=firebase.FirebaseApplication('https://mqtt-raspberry.firebase.com/')
 
-
+parse=argparse.ArgumentParser()
+parser.add_argument("-c","--cascade,required=True,
+                    help"path to where the face cascade)
+parser.add_argument("-e","--encodings,required=True,
+                    help"path to sterialized dbof facial encodings")
+                    args=vars(parser.parse_args())
+             
