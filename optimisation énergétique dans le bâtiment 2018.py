608f9c18f97d56387ac61da9194020882f5b3439
Rania BEN TEKAYA <117775943+bentekaya@users.noreply.github.com>
2022-11-10 21:57:25 +0100

    Update optimisation énergétique dans le bâtiment 2018.py

--- optimisation énergétique dans le bâtiment 2018.py
+++ optimisation énergétique dans le bâtiment 2018.py
@@ -12,7 +12,19 @@ import pandas_profiling
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+from sklearn.preprocessing import Normalizer
+                   
 data.head()
+#corrélation entre les données
 correlation = data.corr()
 plt.figure(figsize=(11,11))
 sns.heatmap(correlation,annot=True)
+plt.figure(figsize=(5,5))
+#corrélation entre les variables
+sns.pairplot(data=data, y_vars=['Charge de chauffage', 'Charge de refroidissement'],
+x_vars=[ 'Surface des murs','Orientation', 'Surface de vitrage'])
+plt.show()
+#prédiction de la charge de refroidissement et de chauffement par la méthade de régression linéaire
+#standarisation 
+                   
+                   
