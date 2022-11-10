a6093a8fc1fd2f3e3eae5bdeb89deb51e733c2b1
Rania BEN TEKAYA <117775943+bentekaya@users.noreply.github.com>
2022-11-10 21:32:22 +0100

    Create optimisation énergétique dans le bâtiment 2018.py

---
+++ optimisation énergétique dans le bâtiment 2018.py
@@ -0,0 +1,12 @@
+#outpout:y1 Charge de chauffage ,y2 Charge de refroidissement
+ #Input:
+#X1: Orientation
+#x2:Surface de vitrage
+#X3 :Surface des murs
+
+
+import pandas as pd
+import pandas_profiling
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
