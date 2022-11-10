4c25263abcf3d4a052c80c36d0e39e24b34c66a9
Rania BEN TEKAYA <117775943+bentekaya@users.noreply.github.com>
2022-11-10 21:43:47 +0100

    Update optimisation énergétique dans le bâtiment 2018.py

--- optimisation énergétique dans le bâtiment 2018.py
+++ optimisation énergétique dans le bâtiment 2018.py
@@ -3,10 +3,16 @@
 #X1: Orientation
 #x2:Surface de vitrage
 #X3 :Surface des murs
-
+#tous les variables x,y sont des variables quantitatives
+#importer les données nécesaite
+data = pd.read_csv(rania.csv')
 
 import pandas as pd
 import pandas_profiling
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+data.head()
+correlation = data.corr()
+plt.figure(figsize=(11,11))
+sns.heatmap(correlation,annot=True)
